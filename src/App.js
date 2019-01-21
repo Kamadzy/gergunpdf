c2a06bbb004370112c635a1fa99a4c979651b1ba
Kamadzy <kam4513@gmail.com>
2019-01-21 17:53:26 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Page1 from "./components/page1/Page1";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Page1 />
+      </div>
+    );
+  }
+}
+
+export default App;
