529736b20db76b9ec6544420f42eba6270ad918d
Kamadzy <kam4513@gmail.com>
2019-01-25 23:50:47 +0200

    Updates

--- precache-manifest.fe334880fd1499f0229c9aa440902baf.js
+++ precache-manifest.fe334880fd1499f0229c9aa440902baf.js
@@ -1,67 +1,71 @@
 self.__precacheManifest = [
   {
-    "revision": "210a7c781f5a354a0e4985656ab456d9",
-    "url": "/gergunpdf/static/media/roboto-latin-300italic.210a7c78.woff"
+    "revision": "9680d5a0c32d2fd084e07bbc4c8b2923",
+    "url": "/gergunpdf/static/media/roboto-latin-400italic.9680d5a0.woff"
   },
   {
-    "revision": "5560c64bd5fa49a06cb7",
-    "url": "/gergunpdf/static/css/main.c819def1.chunk.css"
+    "revision": "e3561792bc6458439b83",
+    "url": "/gergunpdf/static/css/main.5f846f8e.chunk.css"
   },
   {
     "revision": "bc833e725c137257c2c42a789845d82f",
     "url": "/gergunpdf/static/media/roboto-latin-900italic.bc833e72.woff"
   },
   {
-    "revision": "50a947d84edfe065a355",
-    "url": "/gergunpdf/static/js/1.50a947d8.chunk.js"
+    "revision": "8c6b9afcff807bcbb3e6",
+    "url": "/gergunpdf/static/js/1.8c6b9afc.chunk.js"
   },
   {
     "revision": "0302be60a217cc4660b0",
     "url": "/gergunpdf/static/js/runtime~main.0302be60.js"
   },
   {
-    "revision": "6232f43d15b0e7a0bf0fe82e295bdd06",
-    "url": "/gergunpdf/static/media/roboto-latin-100italic.6232f43d.woff2"
+    "revision": "e9dbbe8a693dd275c16d32feb101f1c1",
+    "url": "/gergunpdf/static/media/roboto-latin-100.e9dbbe8a.woff"
   },
   {
     "revision": "987b84570ea69ee660455b8d5e91f5f1",
     "url": "/gergunpdf/static/media/roboto-latin-100.987b8457.woff2"
   },
   {
-    "revision": "e9dbbe8a693dd275c16d32feb101f1c1",
-    "url": "/gergunpdf/static/media/roboto-latin-100.e9dbbe8a.woff"
+    "revision": "6232f43d15b0e7a0bf0fe82e295bdd06",
+    "url": "/gergunpdf/static/media/roboto-latin-100italic.6232f43d.woff2"
   },
   {
     "revision": "d704bb3d579b7d5e40880c75705c8a71",
     "url": "/gergunpdf/static/media/roboto-latin-100italic.d704bb3d.woff"
   },
   {
-    "revision": "55536c8e9e9a532651e3cf374f290ea3",
-    "url": "/gergunpdf/static/media/roboto-latin-300.55536c8e.woff2"
+    "revision": "a1471d1d6431c893582a5f6a250db3f9",
+    "url": "/gergunpdf/static/media/roboto-latin-300.a1471d1d.woff"
   },
   {
     "revision": "d69924b98acd849cdeba9fbff3f88ea6",
     "url": "/gergunpdf/static/media/roboto-latin-300italic.d69924b9.woff2"
   },
   {
-    "revision": "a1471d1d6431c893582a5f6a250db3f9",
-    "url": "/gergunpdf/static/media/roboto-latin-300.a1471d1d.woff"
+    "revision": "55536c8e9e9a532651e3cf374f290ea3",
+    "url": "/gergunpdf/static/media/roboto-latin-300.55536c8e.woff2"
   },
   {
-    "revision": "d8bcbe724fd6f4ba44d0ee6a2675890f",
-    "url": "/gergunpdf/static/media/roboto-latin-400italic.d8bcbe72.woff2"
+    "revision": "210a7c781f5a354a0e4985656ab456d9",
+    "url": "/gergunpdf/static/media/roboto-latin-300italic.210a7c78.woff"
+  },
+  {
+    "revision": "5d4aeb4e5f5ef754e307d7ffaef688bd",
+    "url": "/gergunpdf/static/media/roboto-latin-400.5d4aeb4e.woff2"
   },
   {
     "revision": "bafb105baeb22d965c70fe52ba6b49d9",
     "url": "/gergunpdf/static/media/roboto-latin-400.bafb105b.woff"
   },
   {
-    "revision": "9680d5a0c32d2fd084e07bbc4c8b2923",
-    "url": "/gergunpdf/static/media/roboto-latin-400italic.9680d5a0.woff"
+    "revision": "e3561792bc6458439b83",
+    "url": "/gergunpdf/static/js/main.e3561792.chunk.js"
   },
   {
-    "revision": "5560c64bd5fa49a06cb7",
-    "url": "/gergunpdf/static/js/main.5560c64b.chunk.js"
+    "revision": "d8bcbe724fd6f4ba44d0ee6a2675890f",
+    "url": "/gergunpdf/static/media/roboto-latin-400italic.d8bcbe72.woff2"
   },
   {
     "revision": "285467176f7fe6bb6a9c6873b3dad2cc",
@@ -84,39 +88,35 @@ self.__precacheManifest = [
     "url": "/gergunpdf/static/media/roboto-latin-700.037d8304.woff2"
   },
   {
-    "revision": "19b7a0adfdd4f808b53af7e2ce2ad4e5",
-    "url": "/gergunpdf/static/media/roboto-latin-900.19b7a0ad.woff2"
-  },
-  {
-    "revision": "5d4aeb4e5f5ef754e307d7ffaef688bd",
-    "url": "/gergunpdf/static/media/roboto-latin-400.5d4aeb4e.woff2"
-  },
-  {
     "revision": "cf6613d1adf490972c557a8e318e0868",
     "url": "/gergunpdf/static/media/roboto-latin-700.cf6613d1.woff"
-  },
-  {
-    "revision": "010c1aeee3c6d1cbb1d5761d80353823",
-    "url": "/gergunpdf/static/media/roboto-latin-700italic.010c1aee.woff2"
-  },
-  {
-    "revision": "8c2ade503b34e31430d6c98aa29a52a3",
-    "url": "/gergunpdf/static/media/roboto-latin-900.8c2ade50.woff"
   },
   {
     "revision": "846d1890aee87fde5d8ced8eba360c3a",
     "url": "/gergunpdf/static/media/roboto-latin-700italic.846d1890.woff"
   },
   {
+    "revision": "010c1aeee3c6d1cbb1d5761d80353823",
+    "url": "/gergunpdf/static/media/roboto-latin-700italic.010c1aee.woff2"
+  },
+  {
+    "revision": "19b7a0adfdd4f808b53af7e2ce2ad4e5",
+    "url": "/gergunpdf/static/media/roboto-latin-900.19b7a0ad.woff2"
+  },
+  {
+    "revision": "8c2ade503b34e31430d6c98aa29a52a3",
+    "url": "/gergunpdf/static/media/roboto-latin-900.8c2ade50.woff"
+  },
+  {
     "revision": "7b770d6c53423deb1a8e49d3c9175184",
     "url": "/gergunpdf/static/media/roboto-latin-900italic.7b770d6c.woff2"
   },
   {
-    "revision": "50a947d84edfe065a355",
+    "revision": "8c6b9afcff807bcbb3e6",
     "url": "/gergunpdf/static/css/1.b11cf7ed.chunk.css"
   },
   {
-    "revision": "7af8063e923ed9acb04f39ce457f7efc",
+    "revision": "07f4bdff4cf17cffa5952c25e3b4a65c",
     "url": "/gergunpdf/index.html"
   }
 ];
